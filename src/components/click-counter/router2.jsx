3b655977777a98e271eb02481b37119a39ae5e84
wangchunyang1 <wangchunyang1@yy.com>
2019-04-28 19:15:28 +0800

    feat: 代码提交

--- src/components/click-counter/router2.jsx
+++ src/components/click-counter/router2.jsx
@@ -1,6 +1,8 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { changeName } from '@/store/user-info/action'
 
-export default class App extends Component {
+class App extends Component {
   constructor (props) {
     super(props)
   }
@@ -10,7 +12,26 @@ export default class App extends Component {
       <div>
         <h3>Router user</h3>
         <div>uid:{this.props.match.params.uid}</div>
+        <div>redux store userInfo name: {this.props.userInfo.name}</div>
+        <div>
+          <button type={"button"} onClick={() => this.props.onChangeName(Math.random())} >Change store name</button>
+        </div>
       </div>
     )
   }
 }
+
+export default connect(
+  state => {
+    return {
+      userInfo: state.userInfo
+    }
+  },
+  dispatch => {
+    return {
+      onChangeName (val) {
+        dispatch(changeName(val))
+      }
+    }
+  }
+)(App)
