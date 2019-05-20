2ce31943cd9740077078e1215f0b801f2c7935ca
wangchunyang1 <wangchunyang1@yy.com>
2019-05-20 19:58:11 +0800

    feat: 代码提交

---
+++ src/js/shallowEqual.js
@@ -0,0 +1,9 @@
+const hasOwn = Object.prototype.hasOwnProperty
+
+function is (x, y) {
+  if (x === y) {
+    return x !== 0 || y !== 0 || 1 / x === 1 / y
+  } else {
+    return x !== x && y !== y
+  }
+}
