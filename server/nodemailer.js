f8a32d9bd759c8d96fa4d36690d457fc7092e352
wangcylive <wangcylive@outlook.com>
2021-01-13 22:06:35 +0800

    feat: 增加测试邮箱功能

--- server/nodemailer.js
+++ server/nodemailer.js
@@ -1,7 +1,20 @@
 const express = require('express')
 const nodemailer = require('nodemailer')
+const multer = require('multer')
 const path = require('path')
 
+const storage = multer.diskStorage({
+  destination(req, file, callback) {
+    callback(null, 'nodemailer-upload/')
+  },
+  filename(req, file, callback) {
+    const fileSuffix = file.originalname.split('.').pop()
+    callback(null, Math.floor(Date.now() * Math.random()) + '.' + fileSuffix)
+  }
+})
+const upload = multer({
+  storage
+})
 const app = express()
 
 // const mailTransport = nodemailer.createTransport( {
@@ -14,48 +27,61 @@ const app = express()
 // });
 
 const transport = nodemailer.createTransport({
-  host: "smtp.163.com",
+  host: 'smtp.163.com',
   secureConnection: true,
   port: 465,
   auth: {
-    user: "wangcylive@163.com",
-    pass: "wang87513375"
+    user: 'wangcylive@163.com',
+    pass: 'wang87513375',
   },
 })
 
-app.post('/email', (req, res) => {
-  const img1 = path.resolve(__dirname, '../src/components/nodemailer/img/p2612216881.jpg')
-  const img2 = path.resolve(__dirname, '../src/components/nodemailer/img/p2616169465.jpg')
+app.post('/email', upload.array('file', 10), (req, res) => {
+  console.log(req.files, req.body)
+  res.header('Access-Control-Allow-Origin', req.headers.origin)
+  res.header('Access-Control-Allow-Headers', 'Content-Type,Content-Length, Authorization, Accept,X-Requested-With')
+  res.header('Access-Control-Allow-Methods', 'PUT,POST,GET,DELETE,OPTIONS')
+  res.header('Access-Control-Allow-Credentials', 'true')
+
+  // res.send('ok')
+  // return
+
+  const { subject, text } = req.body
+  const attachments = req.files.map((file, index) => ({ filename: file.originalname, path: file.path, cid: `0000000${index}` }))
+
   const options = {
     from : 'wangcylive@163.com',
     to: 'wangcylive@outlook.com, 136577647@qq.com',
     // cc         : ''  //抄送
     // bcc      : ''    //密送
-    subject: '一封来自Node Mailer的邮件',
-    text: '一封来自Node Mailer的邮件',
-    html: '<h1>你好，这是一封来自NodeMailer的邮件！</h1><p>hhh</p>',
-    attachments: [
-      {
-        filename: 'img1.jpg',            // 改成你的附件名
-        path: img1,  // 改成你的附件路径
-        cid : '00000001'                 // cid可被邮件使用
-      },
-      {
-        filename: 'img2.jpg',            // 改成你的附件名
-        path: img2,  // 改成你的附件路径
-        cid : '00000002'                 // cid可被邮件使用
-      },
-    ]
+    subject,
+    text,
+    attachments
+    // html: '<h1>你好，这是一封来自NodeMailer的邮件！</h1><p>hhh</p>',
+    // attachments: [
+    //   {
+    //     filename: 'img1.jpg',            // 改成你的附件名
+    //     path: img1,  // 改成你的附件路径
+    //     cid : '00000001'                 // cid可被邮件使用
+    //   },
+    //   {
+    //     filename: 'img2.jpg',            // 改成你的附件名
+    //     path: img2,  // 改成你的附件路径
+    //     cid : '00000002'                 // cid可被邮件使用
+    //   },
+    // ]
   }
-  res.header("Access-Control-Allow-Origin", req.headers.origin);
-  res.header("Access-Control-Allow-Headers", "Content-Type,Content-Length, Authorization, Accept,X-Requested-With");
-  res.header("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");
-  res.header("Access-Control-Allow-Credentials","true");
+
+  console.log(options)
+  // res.send('ok')
+  // return
+
 
   transport.sendMail(options).then(() => {
     res.send('ok')
   }).catch((err) => {
     console.log(err)
+    res.status(500)
     res.send('error')
   })
 })
--- src/components/nodemailer/index.jsx
+++ src/components/nodemailer/index.jsx
@@ -1,23 +1,61 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { hot } from 'react-hot-loader/root'
 
-const NotificationDemo = () => {
-  useEffect(() => {
+const Nodemailer = () => {
+  const [subject, setSubject] = useState('')
+  const [text, setText] = useState('')
+  const [images, setImages] = useState([])
 
-  }, [])
+  const changeSubject = (event) => {
+    setSubject(event.target.value.trim())
+  }
+  const changeText = (event) => {
+    setText(event.target.value.trim())
+  }
+  const changeImages = (event) => {
+    const files = event.target.files
+    setImages([...files])
+  }
+  const onSubmit = (event) => {
+    event.preventDefault()
+    const formData = new FormData()
+    formData.append('subject', subject)
+    formData.append('text', text)
+    images.forEach((file) => {
+      formData.append('file', file)
+    })
 
-  const onClick = () => {
     window.fetch('http://localhost:13000/email', {
-      method: 'POST'
+      method: 'POST',
+      body: formData
     }).then((res) => {
       console.log(res)
     })
+
+    console.log(formData)
   }
 
 
   return <div>
-    <button onClick={onClick}>发送邮件</button>
+    <form onSubmit={onSubmit}>
+      <div>
+        <label>Subject:</label>
+        <input onInput={changeSubject}/>
+      </div>
+      <div>
+        <label htmlFor="">Text:</label>
+        <input type="text" onInput={changeText}/>
+      </div>
+      <div>
+        <label htmlFor="">Images:</label>
+        <input type="file" multiple accept="image/jpeg,image/png" onChange={changeImages}/>
+      </div>
+      <div>
+        <button type="submit">Submit</button>
+      </div>
+    </form>
+    {/*<button onClick={onClick}>发送邮件</button>*/}
   </div>
 }
 
-export default hot(NotificationDemo)
+export default hot(Nodemailer)
